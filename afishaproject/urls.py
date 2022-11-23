9c657671c2669492c8467d3b692418234e59da7d
Dmitry <ko6pa2@gmail.com>
2022-11-23 23:16:45 +1000

    Add frontend

--- afishaproject/urls.py
+++ afishaproject/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from testapp.views import show_page
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_page),

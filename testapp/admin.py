50bc328be3c766c49a763aeeedc79c1a91db0ce0
Dmitry <ko6pa2@gmail.com>
2022-11-24 10:18:12 +1000

    add Place model

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from testapp.models import Place
+
 # Register your models here.
+
+admin.site.register(Place)
--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Place(models.Model):
+
+    title = models.CharField(max_length=50, verbose_name='Название места')
+    title_article = models.TextField(max_length=200, verbose_name='Короткое описание')
+    text_article = models.TextField(verbose_name='Полное описание места')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
